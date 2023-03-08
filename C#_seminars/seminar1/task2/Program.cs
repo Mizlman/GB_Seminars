4eb6864b392ba683c2758eee3e5feeb07aed1f98
alexander.mizulin <alexander.mizulin@gmail.com>
2023-03-08 13:09:42 +0300

    Seminar1_task2

---
+++ C#_seminars/seminar1/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// вывести день недели по заданному числу
+Console.WriteLine("введите номер для дня недели от 1 до 7");
+int num = Convert.ToInt32 (Console.ReadLine());
+switch(num) {
+case 1:
+    Console.WriteLine("День недели - Понедельник");
+    break;
+case 2:
+    Console.WriteLine("День недели - Вторник");
+    break;
+case 3:
+    Console.WriteLine("День недели - Среда");
+    break;
+case 4:
+    Console.WriteLine("День недели - Четверг");
+    break;
+case 5:
+    Console.WriteLine("День недели - Пятница");
+    break;
+case 6:
+    Console.WriteLine("День недели - Суббота");
+    break;
+case 7:
+    Console.WriteLine("День недели - Воскресенье");
+    break;
+default:
+    Console.WriteLine("Введен неправильный номер");
+    break;
+}
