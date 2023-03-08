021e1c43246400e83d0f9291d924193436c73fa8
alexander.mizulin <alexander.mizulin@gmail.com>
2023-03-08 14:26:19 +0300

    Seminar1_task4

---
+++ C#_seminars/seminar1/task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// // Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+Console.WriteLine("введите трехзначное число");
+int num = Convert.ToInt32 (Console.ReadLine());
+if (num >99 && num <1000) { 
+    int result= num%10;
+Console.WriteLine(result);}
+else
+Console.WriteLine("неккоректный ввод");
