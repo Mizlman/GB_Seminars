018c8e0c674c0667a7fa044c921f3bdcf8f56ada
alexander.mizulin <alexander.mizulin@gmail.com>
2023-03-08 12:48:44 +0300

    Seminar1_task1

---
+++ C#_seminars/seminar1/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// ввести два числа и проверить что первое является квадратов второго
+Console.WriteLine("введите первое число");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("введите второе число");
+int b = Convert.ToInt32 (Console.ReadLine());
+if (a==b*b) {
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+Console.WriteLine("Первое число не является квадратом второго");
